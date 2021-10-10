31e7dcf0a3eea27cdd503ca3144a4df067e18074
st091446 <90966286+st091446@users.noreply.github.com>
2021-10-10 09:37:57 -0700

    Add files via upload

---
+++ 2021.10.03-Homework-3/Project-1/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    int n = 0;
+    int i = 1;
+    int f = 0;
+    f = 1;
+    cin >> n;
+    cout << endl;
+    for (int i = 1; i <= n; i++)
+    {
+        f = f * i;
+    }
+    cout << " " << f << endl;
+
+    return EXIT_SUCCESS;
+}
