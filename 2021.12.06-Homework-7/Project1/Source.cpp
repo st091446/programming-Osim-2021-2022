cdaf9c5d8bbe98f31a8509abc73688ecd0d4fff9
st091446 <90966286+st091446@users.noreply.github.com>
2021-12-14 21:05:21 +0300

    Homework-7

---
+++ 2021.12.06-Homework-7/Project1/Source.cpp
@@ -0,0 +1,17 @@
+#include"mylib.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+
+	int n = 0;
+	cin >> n;
+	cout << f(n) << endl;
+
+	int r = 0;
+	cin >> r;
+	round(r);
+
+	return EXIT_SUCCESS;
+}
